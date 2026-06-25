83367583336c7c1b596fab5ae814ff25826259f4
Lucienne Ouedraogo <lucienneo133@gmail.com>
2026-06-25 02:45:45 +0200

    Creation de la table de donnees des candidatures

---
+++ app_candidatures/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppCandidaturesConfig(AppConfig):
+    name = 'app_candidatures'
---
+++ app_candidatures/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+class Candidature(models.Model):
+    dateSoumission = models.DateTimeField(auto_now_add=True)#enregistre le jour et l'heure exact ou le candidat clique sur le bouton Postueler,il prends l'heure de l'ordinateur automatiquement 
+    status=models.CharField(max_length=50,default='En attente')#cree un texte court dans la base de donnees pour que qund une candiadature arrive django  remplit automatiquement la case en attente
+    dateModification = models.DateTimeField(auto_now =True)# met a jpour automatiquement la date a chaque fois que quelqu'un touche a la candidature
+     # Relie la candidature à la table Candidat. Si le candidat est supprimé, la candidature s'efface (CASCADE).
+      # 'candidat' (minuscule) accueillera l'instance réelle (le vrai utilisateur connecté).
+    # 'Candidat' (majuscule) désigne la classe générale (le plan ou la table cible).
+    # --- RELATIONS TEMPORAIRES (Pour éviter de bloquer le projet) ---
+    # On remplace les flèches par du texte pour stocker temporairement les identifiants
+    candidat_id = models.CharField(max_length=50, blank=True, null=True)
+    offre_id = models.CharField(max_length=50, blank=True, null=True)
+
+def __str__(self):
+    return f'Candidature {self.id} - Statut: {self.status}'
+
