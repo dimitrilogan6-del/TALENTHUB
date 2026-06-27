8c45a7005250cbb98cf0dd826352c93b65148081
Lucienne Ouedraogo <lucienneo133@gmail.com>
2026-06-27 17:19:44 +0200

    Mise en place de la validation anti-doublon dans le Serializer des candidatures

--- app_candidatures/serializers.py
+++ app_candidatures/serializers.py
@@ -4,3 +4,16 @@ class CandidatureSerializer(serializers.ModelSerializer):# Étape 3 : On crée l
     class Meta:#On ouvre la configuration interne du traducteur
         model = Candidature#On dit au traducteur de travailler sur ton modèle Candidature
         fields = '__all__'#On lui demande d'inclure absolument toutes les colonnes dans l'API
+    def validate(self,data) :
+        #On extrait l'dentification du candidadt et de l'offre 
+         candidat_id = data.get('candidat_id')  
+         offre_id = data.get('offre_id')
+         #on interroge la base de donnee  s'il existe deja ce candidat avec cet offre
+         deja_postule = Candidature.objects.filter(candidat_id=candidat_id, offre_id=offre_id).exists()
+
+         if deja_postule:
+             #fais des recherches dans la base de donnees 
+             
+             raise serializers.ValidationError(
+             {"erreur":"vous avez deja soumis une candidature pour cette offre"})
+             return data
