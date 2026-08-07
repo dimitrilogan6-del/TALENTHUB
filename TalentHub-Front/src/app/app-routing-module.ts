eeab7ad6b09671522312e5da65a4a09437fa8fca
fallndeyeseynabou821 <fallndeyeseynabou821@gmail.com>
2026-08-07 22:39:10 +0000

    ajout page d'accueil

--- TalentHub-Front/src/app/app-routing-module.ts
+++ TalentHub-Front/src/app/app-routing-module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CandidatureModal } from './component/candidature-modal/candidature-modal';
 import{ OffreList } from './component/offre-list/offre-list';
 import{CandidatPage} from './component/candidat-page/candidat-page';
+import { Accueil } from './component/accueil/accueil';
 const routes: Routes =[
-  { path: '', redirectTo: '/candidat',pathMatch: 'full' },
+  { path: '', redirectTo: '/accueil',pathMatch: 'full' },
+  { path: 'accueil', component: Accueil },
   { path: 'candidat', component: CandidatPage },
   { path: 'offre', component: OffreList },
   { path: 'candidature', component: CandidatureModal },
+  
 ]
 
 @NgModule({
--- TalentHub-Front/src/app/app.module.ts
+++ TalentHub-Front/src/app/app.module.ts
@@ -8,8 +8,9 @@ import { App } from './app';
 import { OffreList } from './component/offre-list/offre-list';
 import { CandidatPage } from './component/candidat-page/candidat-page';
 import { CandidatureModal } from './component/candidature-modal/candidature-modal';
+import { Accueil } from './component/accueil/accueil';
 @NgModule({
-  declarations: [App, OffreList, CandidatPage, CandidatureModal],
+  declarations: [App, OffreList, CandidatPage, CandidatureModal, Accueil],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, HttpClientModule],
   providers: [],
   bootstrap: [App],
